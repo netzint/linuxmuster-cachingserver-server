6a8dbb90c614713dd629b52ed3fa277f46b3b333
hermanntoast <lukas.spitznagel@netzint.de>
2024-04-26 12:05:04 +0000

    Add sync after import devices

---
+++ usr/lib/linuxmuster-cachingserver/hook-executor-import-devices.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#########################################################
+# 
+# by Netzint GmbH 2024
+# Lukas Spitznagel (lukas.spitznagel@netzint.de)
+# 
+#########################################################
+
+SCHOOL=$2
+
+echo "Initiating synchronization on satellite-server..."
+/usr/bin/linuxmuster-cachingserver sync $@
